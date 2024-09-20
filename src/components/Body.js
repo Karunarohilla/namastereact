3c6e1e7e6713e921c9c69321167230423fa8d001
Karuna Rohilla <karunarohilla95@gmail.com>
2024-09-20 12:26:44 +0530

    Show image, name, location etc on page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export default Body = () => {
-
+    
+    const [restaurantCard, setRestaurantCard] = useState([]);
     useEffect (()=>{
         console.log('use effect');
         fetchData();
     }, []);
-
 
     const fetchData = async() =>{
         const url = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.89640&lng=76.59090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
@@ -17,14 +17,15 @@ export default Body = () => {
             }
             const json = await response.json();
             console.log(json)
-            const details = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants
+            const details = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants || []
             // console.log('line 22', json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants)
             console.log('line 23', details[0].info)
 
             console.log('line 24',details[0]?.info?.id, details[0]?.info?.name, details[0]?.info?.cloudinaryImageId, details[0]?.info?.locality, details[0]?.info?.areaName)
 
-
-            const {id, name, cloudinaryImageId, locality, areaName} = details[0]?.info;
+            setRestaurantCard(details);
+            console.log('line 27', details)
+            const {id, name, cloudinaryImageId, locality, areaName, avgRating} = details[0]?.info;
         }
         catch(error){
             console.log(error);
@@ -35,25 +36,24 @@ export default Body = () => {
       <div className="container mt-3">
         <h2>Card titles, text, and links</h2>
         <div className="row">
-             
-            <div className="col-md-3">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Card title</h4>
+            {restaurantCard.map((restaurantMenu, key) =>{
+                return(
+                    <div className="col-md-3" key={key}>
+                        <div className="card-header">
+                            <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`+restaurantMenu?.info?.cloudinaryImageId} className="img-fluid" alt="Cloudinary image" />
+                        </div>
+                        <div className="card-body">
+                            <h5>{restaurantMenu?.info?.name}</h5>
+                            <p>{restaurantMenu?.info?.locality}</p>
+                            <h6>{restaurantMenu?.info?.areaName}</h6>
+                            <p>Rating: <b>{restaurantMenu?.info?.avgRating}</b></p>
+                        </div>
                         
-                        <p className="card-text">
-                        
-                        </p>
-                        <a href="#" className="card-link">
-                        Card link
-                        </a>
-                        <a href="#" className="card-link">
-                        Another link
-                        </a>
                     </div>
-                </div>
-            </div>
-            
+
+                    
+                ) 
+            })}
         </div>
       </div>
     </>
